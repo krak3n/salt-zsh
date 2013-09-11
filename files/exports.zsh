06599ea6e3ffbe887505d0cc178345b5dbf4da40
Chris Reeves <hello@chris.reeves.io>
2013-09-11 09:29:51 +0100

    Virtualenv Logging

--- files/exports.zsh
+++ files/exports.zsh
@@ -13,3 +13,6 @@ export LC_ALL=en_US.UTF-8
 export LC_CTYPE=en_US.UTF-8
 export LANG=en_US.UTF-8
 export LANGUAGE=en_US.UTF-8
+
+# Virtualenv Logging
+export VIRTUALENVWRAPPER_LOG_FILE="$HOME/.virutalenv.log"
